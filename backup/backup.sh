7730fb8812ea201602fbc0964941a433df9018a8
Pantelis Zoupis <pantelis.zoupis@gmail.com>
2024-09-13 01:23:49 +0300

    Add manual mode

--- backup/backup.sh
+++ backup/backup.sh
@@ -14,6 +14,9 @@ backup()
         snapshots)
             _backup_snapshots
             ;;
+        manual)
+            _backup_manual
+            ;;
         help)
             _backup_usage
             ;;
@@ -28,6 +31,7 @@ _backup_usage()
     echo "COMMANDS"
     echo "  backup: creates a backup"
     echo "  snapshots: prints the snapshots"
+    echo "  manual: Loads environment variables for manual use of restic"
     echo "  help: prints this help"
     echo
 }
@@ -84,6 +88,33 @@ _backup_snapshots()
         set -a
         . $environment_variables_directory/$opt.env
         restic snapshots
+        break
+    done
+}
+
+_backup_manual()
+{
+    # Directory of files with restic environment variables
+    environment_variables_directory=~/Documents/AppFiles/restic
+
+    files_array=()
+    backup_location_array=()
+
+    for entry in "$environment_variables_directory"/*
+    do
+        filename_with_extension=$(basename "$entry")
+        if [[ $filename_with_extension == *.env ]]; then
+            filename="${filename_with_extension%.*}"
+            array+=($filename_with_extension)
+            backup_location_array+=($filename)
+        fi
+    done
+
+    PS3='Select backup location: '
+    options=${backup_location_array[*]}
+    select opt in "${backup_location_array[@]}"; do
+        set -a
+        . $environment_variables_directory/$opt.env
         break
     done
 }
